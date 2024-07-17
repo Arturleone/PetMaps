dbb09c83dcbae6adcef6e03d026299ca9d8404f7
Artur Leonel <arturleonel08@hotmail.com>
2024-07-17 15:08:13 -0300

    Reorganizado telas

--- app/src/main/java/com/example/petmaps/MainActivity.kt
+++ app/src/main/java/com/example/petmaps/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.petmaps
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,5 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+        Handler().postDelayed({
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
