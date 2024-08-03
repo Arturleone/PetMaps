180703d1f04a200d8ac792691c48f7d65bc53fc4
artur <you@example.com>
2024-08-03 20:14:35 -0300

    Correção Tela Inicial

--- app/src/main/java/com/example/petmaps/Telainicial.kt
+++ app/src/main/java/com/example/petmaps/Telainicial.kt
@@ -10,52 +10,9 @@ import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
-class Telainicial : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
-    private lateinit var drawerLayout: DrawerLayout
-
-    @SuppressLint("MissingInflatedId")
+class Telainicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_base)
-
-        drawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val menu = findViewById<ImageView>(R.id.Bolonha)
-
-        menu.setOnClickListener {
-            drawerLayout.openDrawer(GravityCompat.START)
-        }
-
-        navView.setNavigationItemSelectedListener { menuItem ->
-            menuItem.isChecked = true
-            drawerLayout.closeDrawers()
-
-            when (menuItem.itemId) {
-                R.id.nav_home -> {
-                    startActivity(Intent(this, Login::class.java))
-                }
-                R.id.nav_worldskills -> {
-                    // Handle the worldskills action
-                }
-                R.id.nav_contact -> {
-                    // Handle the contact action
-                }
-                R.id.nav_settings -> {
-                    // Handle the settings action
-                }
-            }
-
-            true
-        }
-    }
-
-    override fun onBackPressed() {
-
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        }else{
-            super.onBackPressed()
-        }
+        setContentView(R.layout.activity_telainicial)
     }
 }
